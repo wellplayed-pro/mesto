ce844010bb250d4487584d19e9126a1424fa1373
Artem Khomutinnikov <89296212+wellplayed-pro@users.noreply.github.com>
2023-03-01 19:41:14 +0300

    fetch skeleton

---
+++ scripts/validate.js
@@ -0,0 +1,44 @@
+const formElement = document.querySelector('.popup');
+const inputElement = formElement.querySelector('.popup__input');
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input-error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error-visible');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input-error');
+  errorElement.classList.remove('popup__input-error-visible');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+function enableValidation () {
+  const formList = Array.from(formElement.querySelectorAll('.popup'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function () {
+      setEventListeners(formElement);
+    });
+     });
+}
+enableValidation();
