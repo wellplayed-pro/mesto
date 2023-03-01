f87fe38dbff73bc78cf599b69d70b8fb566e3598
Artem Khomutinnikov <89296212+wellplayed-pro@users.noreply.github.com>
2023-03-01 19:43:39 +0300

    fix skeleton 0.01

--- scripts/validate.js
+++ scripts/validate.js
@@ -41,4 +41,11 @@ function enableValidation () {
     });
      });
 }
-enableValidation();
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}); 
