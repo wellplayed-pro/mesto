d3b7c8b0abd0b75e2e613af54f03f23908e0c72d
Artem Khomutinnikov <89296212+wellplayed-pro@users.noreply.github.com>
2023-03-01 22:34:44 +0300

    fix again,,,last css

--- scripts/validate.js
+++ scripts/validate.js
@@ -27,7 +27,7 @@ const handleFormInput = (evt, config) => {
   const input = evt.target;
   const inputId = input.id;
   const errorElement = document.querySelector(`#${inputId}-error`);
-
+console.log(errorElement);
   if (input.validity.valid) {
     input.classList.remove(config.inputErrorClass)
     errorElement.textContent = '';
